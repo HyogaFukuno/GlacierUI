527eb59bc098492128ab24b9aaca3cdb5bdd3500
HyogaFukuno <hyoga.fukuno521@gmail.com>
2023-06-23 19:23:03 +0900

    MonoBehaviour の最適化を目的としたクラス GlacierBehaviour の作成

---
+++ Assets/Plugins/Glacier/Scripts/Common/MonoBehaviour/GlacierBehaviour.cs
@@ -0,0 +1,50 @@
+﻿namespace Glacier.Common.MonoBehaviour
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// 各プロパティのキャッシュや独自の処理を実装したMonoBehaviourクラス。
+    /// </summary>
+    public class GlacierBehaviour : MonoBehaviour
+    {
+        #region variables
+
+        private GameObject _gameObjectCache;
+
+        private Transform _transformCache;
+
+        #endregion
+
+        #region properties
+
+        public new GameObject gameObject
+        {
+            get
+            {
+                if (null == _gameObjectCache)
+                {
+                    _gameObjectCache = base.gameObject;
+                }
+
+                return _gameObjectCache;
+            }
+        }
+
+        public new Transform transform
+        {
+            get
+            {
+                if (null == _transformCache)
+                {
+                    _transformCache = base.transform;
+                }
+
+                return _transformCache;
+            }
+        }
+
+        #endregion
+    }
+}
